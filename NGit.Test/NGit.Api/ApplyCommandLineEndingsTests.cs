154ae64ce022f2d6087df62d226e6720821c6c41
Peter Gerrard <peter.gerrard@red-gate.com>
2016-08-01 15:58:42 +0100

    Pull out GetPatch method in test.

--- NGit.Test/NGit.Api/ApplyCommandLineEndingsTests.cs
+++ NGit.Test/NGit.Api/ApplyCommandLineEndingsTests.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text;
 using NUnit.Framework;
 
 namespace NGit.Api
@@ -11,22 +12,33 @@ namespace NGit.Api
             const string pre = "a\r\r\nb";
             const string post = "a\r\nb";
             var git = new Git(db);
-            var stream = new MemoryStream();
             var modifiedFile = Path.Combine(db.WorkTree, "file.txt");
 
             File.WriteAllText(modifiedFile, pre);
             git.Add().AddFilepattern(".").Call();
             git.Commit().SetMessage("Initial commit").Call();
 
-            File.WriteAllText(modifiedFile, post);
-            git.Diff().SetOutputStream(stream).Call();
+            var patch = GetPatch(modifiedFile, pre, post, git);
 
-            stream.Position = 0;
-            File.WriteAllText(modifiedFile, pre);
-
-            git.Apply().SetPatch(stream).Call();
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(patch)))
+            {
+                git.Apply().SetPatch(stream).Call();
+            }
 
             Assert.That(File.ReadAllText(modifiedFile), Is.EqualTo(post));
+        }
+
+        private static string GetPatch(string modifiedFile, string originalText, string newText, Git git)
+        {
+            using (var stream = new MemoryStream())
+            {
+                File.WriteAllText(modifiedFile, newText);
+                git.Diff().SetOutputStream(stream).Call();
+                
+                File.WriteAllText(modifiedFile, originalText);
+
+                return Encoding.ASCII.GetString(stream.ToArray());
+            }
         }
     }
 }
